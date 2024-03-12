bce5408a88b31f4831b2b65005402480586d881a
Nopfed <robby.tsunami@gmail.com>
2024-03-11 22:21:27 -0400

    WIP: 404s done, working on other statuses and callbackURL (#83)
    * WIP: 404s done, working on other statuses and callbackURL
    
    * WIP: Added callbackURL test
    
    * WIP: Working on setting request headers

--- test/spec/uploads.spec.ts
+++ test/spec/uploads.spec.ts
@@ -43,10 +43,33 @@ describe('Uploads', () => {
       expect(res).to.have.status(401)
     })
 
-    it('Returns upload id on upload')
-    it('HTTP 411 Length Required')
-    it('HTTP 413 Content Too Large')
-    it('HTTP 415 Unsupported Media Type')
+    it('Returns upload id on upload',
+      // have mock file upload
+      // check header for file
+      // check for 200 status
+
+    )
+
+    it('HTTP 411 Length Required', async () => {
+      // const res = await chai
+      //   .request(api.server)
+      //   .post(route)
+      //   .set('content-type', 'video/mp4')
+      //   .query({ token: mockApiKeys[0].apiKey })
+      //   .send()
+
+      // // console.log(res)
+      // expect(res).to.have.status(411)
+    })
+    
+    it('HTTP 413 Content Too Large', 
+      // check size of files in request against file size limit
+    )
+
+    it('HTTP 415 Unsupported Media Type', 
+      // check file for correct type, fail on all others
+    )
+
     it('HTTP 500')
   })
 
@@ -92,7 +115,16 @@ describe('Uploads', () => {
       expect(res.body.upload).to.exist
     })
 
-    it('HTTP 404')
+    it('HTTP 404', async () => {
+      const route = '/nerf/uploads/foo'
+      const res = await chai
+        .request(api.server)
+        .get(route)
+        .query({ token: mockApiKeys[0].apiKey })
+      
+      expect(res).to.have.status(404)
+    })
+
     it('HTTP 500')
   })
 
@@ -123,8 +155,34 @@ describe('Uploads', () => {
       })
     })
 
-    it('Callback URL tests')
-    it('HTTP 404')
+    it('Callback URL tests', async () => {
+      const res = await chai
+        .request(api.server)
+        .post(route)
+        .query({ token: mockApiKeys[0].apiKey })
+        .send({
+          callbackURL: 'http://localhost:3000'
+        })
+      
+        
+      expect(res).to.have.status(200)
+      // console.log(res.body)
+      expect(res.body.apiKey).to.equal(mockApiKeys[0].apiKey)
+      expect(res.body.jobData.callbackURL).to.exist
+      expect(res.body.jobData.callbackURL).to.be.a('string')
+      // does this need to do anything else?
+    })
+
+    it('HTTP 404', async () => {
+      const route = '/nerf/uploads/foo'
+      const res = await chai
+        .request(api.server)
+        .get(route)
+        .query({ token: mockApiKeys[0].apiKey })
+      
+      expect(res).to.have.status(404)
+    })
+
     it('HTTP 500')
   })
 })
